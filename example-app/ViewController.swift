2672887f22ee42cfa7d7106b32487551a56c7e8c
smshin7 <scottshin1@gmail.com>
2015-06-12 12:33:05 -0700

    Initial commit

---
+++ example-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  example-app
+//
+//  Created by Scott Shin on 6/12/15.
+//  Copyright (c) 2015 Scott Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
